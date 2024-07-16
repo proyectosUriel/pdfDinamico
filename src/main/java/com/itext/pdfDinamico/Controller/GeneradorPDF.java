333b3d1dfcd531b58877e8dd05a2072b8f36bb97
uriel.raigon <uriel.raigon@madisonmk.com>
2024-07-16 14:54:41 +0200

    crearEstructura2

--- src/main/java/com/itext/pdfDinamico/Controller/GeneradorPDF.java
+++ src/main/java/com/itext/pdfDinamico/Controller/GeneradorPDF.java
@@ -1,5 +1,27 @@
 package com.itext.pdfDinamico.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itext.pdfDinamico.Model.DatosRecibir;
+
+@RestController
+@RequestMapping("/api/pdf")
 public class GeneradorPDF {
 
+    @PostMapping("/generate")
+    public ResponseEntity<String> generatePDF(@RequestBody DatosRecibir datos){
+
+        String docpdf = datos.getDocpdf();
+        String datosInsertar= datos.getDatosInsertar();
+        String textoBuscar= datos.getTextoBuscar();
+
+        String respuesta="PDF "+docpdf+"\n Datos insertar "+datosInsertar+"\n Texto a buscar "+textoBuscar;
+
+        return new ResponseEntity<>(respuesta, HttpStatus.OK);
+    }
 }
--- src/main/java/com/itext/pdfDinamico/Model/DatosRecibir.java
+++ src/main/java/com/itext/pdfDinamico/Model/DatosRecibir.java
@@ -2,4 +2,43 @@ package com.itext.pdfDinamico.Model;
 
 public class DatosRecibir {
 
+    private String docpdf;
+    private String datosInsertar;
+    private String textoBuscar;
+
+    // Constructor por defecto
+    public DatosRecibir() {
+    }
+    
+    // Constructor
+    public DatosRecibir(String docpdf, String datos, String textoBuscar) {
+        this.docpdf = docpdf;
+        this.datosInsertar = datos;
+        this.textoBuscar = textoBuscar;
+    }
+
+    // Getters & Setters
+    public String getDocpdf() {
+        return docpdf;
+    }
+
+    public void setDocpdf(String docpdf) {
+        this.docpdf = docpdf;
+    }
+
+    public String getDatosInsertar() {
+        return datosInsertar;
+    }
+
+    public void setDatosInsertar(String datos) {
+        this.datosInsertar = datos;
+    }
+
+    public String getTextoBuscar() {
+        return textoBuscar;
+    }
+
+    public void setTextoBuscar(String textoBuscar) {
+        this.textoBuscar = textoBuscar;
+    }
 }

f956b5782bac4257985ffafccff07dda07067186
uriel.raigon <uriel.raigon@madisonmk.com>
2024-07-16 13:42:59 +0200

    primer commit pdf dinamico

---
+++ src/main/java/com/itext/pdfDinamico/PdfDinamicoApplication.java
@@ -0,0 +1,13 @@
+package com.itext.pdfDinamico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfDinamicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfDinamicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itext/pdfDinamico/PdfDinamicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itext.pdfDinamico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfDinamicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
